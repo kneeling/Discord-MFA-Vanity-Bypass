bea23af0de47971729ee1d0c17fdb1a38cf189bb
kneeling <155864385+kneeling@users.noreply.github.com>
2025-01-02 15:48:34 -0600

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ class MFA:
     
         self.ticket = self.get_mfa_ticket()
         print(f"Ticket: {self.ticket}")
-        print(f"Vanity Token response: {self.get_vanity_token()}")
+        self.mfa_auth = self.get_vanity_token()
+        print(f"Vanity Token response: {self.mfa_auth}")
 
     def get_mfa_ticket(self):
         return self.session.patch(
@@ -35,6 +36,15 @@ class MFA:
             },
             json={"data": self.password, "mfa_type": "password", "ticket": self.ticket},
         ).json()["token"]
+        
+    def set_vanity(self): # wont be called unless user wants to change vanity
+        return self.session.patch(f"https://discord.com/api/v9/guilds/{self.guild_id}/vanity-url", headers={
+            "Authorization": self.token,
+            "x-super-properties": self.super_properties,
+            "x-discord-mfa-authorization": self.mfa_auth,
+            "Cookie": f"__Secure-recent_mfa={self.mfa_auth};",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.330 Chrome/128.0.6613.186 Electron/32.2.7 Safari/537.36"
+        }, json={"code": self.vanity_code}).json()
 
 
 init = MFA()
